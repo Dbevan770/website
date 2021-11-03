44895f7ca8a646c4faeaa9535f852b3376028c90
Danny <Gingerbreadpk@gmail.com>
2021-11-03 11:04:36 +0100

    Uploaded source code and image files to build the website.

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const app = express();
+const server = http.createServer(app);
+const port = 3000;
+
+app.use(express.static(path.join(__dirname + '/public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', '/main.html'));
+});
+app.get('/forums', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', '/forums.html'));
+});
+app.get('/admins', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', '/admins.html'));
+});
+app.get('/servers', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', '/servers.html'));
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', '/about.html'));
+});
+
+
+server.on('listening',function(){
+    console.log(`HTTP Server listening on port ${port}...`);
+});
+
+server.listen(port);
